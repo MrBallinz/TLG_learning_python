2e4934c6e81ebfe80a19913ff8b547326b026472
MrBallinz <68559009+MrBallinz@users.noreply.github.com>
2020-07-22 13:15:04 -0700

    Add files via upload

---
+++ day_three_labs.py
@@ -0,0 +1,37 @@
+#Create a file called day_three.py
+
+#In the file solve the following problems
+
+
+#1) Create two variables a, b that's set equal to some numbers. Print the following:
+
+a = 5
+b = 6
+
+print(a + b)
+print(a * b)
+print(a - b)
+print(a / b)
+print(a % b)
+
+
+#2) Create a string x and set it equal to 'python'. Do the following on the string:
+x='python'
+print(x[0]) #the element at the 0th index
+print(x[-1]) #the element at the last index
+print(len(x)) #the size of the string
+x = x.upper() #uppercase the letters in the string by using the upper() method
+print(x)
+
+
+nums= [] # Create a list called nums that by default is empty. Do the following:
+nums.append(10)# Add 10
+nums.append(100)# Add 100
+nums.append(10)# Add 10
+nums.append(500)# Add 500
+nums.append(56)# Add 56
+nums.pop(56)# the last element
+nums.sort() # Sort the last element
+print(nums)
+
+#Once done, upload to GitHub and post your GitHub repo in the Slack channel.
